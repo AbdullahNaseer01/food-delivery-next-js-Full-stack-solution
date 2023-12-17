c7db371c829681124fbd34c67d9f107661b47a89
abdullahnaseer01 <mianabdullah125125@gmail.com>
2023-12-18 00:19:32 +0500

    error work close

---
+++ src/app/(ADMIN)/adminpanel/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from '../(admin-components)/sidebar'
+import ProductForm from '../(admin-components)/ProductForm'
+import ProductTable from '../(admin-components)/ProductTable'
+
+const page = () => {
+  return (
+    <main className=" sm:ml-60 pt-16  max-h-screen overflow-auto bg-slate-400 min-h-screen">
+        {/* <ProductForm /> */}
+        <ProductTable/>
+      </main>
+  )
+}
+
+export default page
--- src/app/(ADMIN)/layout.js
+++ src/app/(ADMIN)/layout.js
@@ -1,6 +1,6 @@
 
 import "@/app/globals.css"
-import Sidebar from "@/app/(admin-panel)/(admin-components)/Sidebar";
+import Sidebar from "@/app/(ADMIN)/(admin-components)/Sidebar";
 import AdminHeader from "./(admin-components)/Adminheader";
 
 export default function adminlayout({ children }) {

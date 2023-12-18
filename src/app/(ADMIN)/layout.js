e68dfddf06dd30579e614694f1f8b1efff629caa
abdullahnaseer01 <mianabdullah125125@gmail.com>
2023-12-18 16:39:17 +0500

    refactoring of layout and folder structuring

--- src/app/(ADMIN)/layout.js
+++ src/app/(ADMIN)/layout.js
@@ -1,21 +1,3 @@
-
-// import "@/app/globals.css"
-// import Sidebar from "@/app/(ADMIN)/(admin-components)/Sidebar";
-// import AdminHeader from "./(admin-components)/Adminheader";
-
-// export default function adminlayout({ children }) {
-//     return (
-//         <section>
-//             <AdminHeader/>
-//             <Sidebar />
-//             {children}
-//         </section>
-
-
-//     )
-// }
-
-
 import { Inter } from 'next/font/google'
 import '@/app/globals.css'
 import AdminHeader from './(admin-components)/Adminheader'

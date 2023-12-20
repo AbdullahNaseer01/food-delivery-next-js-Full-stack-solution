00af8ef47079217d0eb4e428096e20eb77699f87
abdullahnaseer01 <mianabdullah125125@gmail.com>
2023-12-21 01:25:12 +0500

    nextauth (login-lgout)

--- src/app/api/auth/[...nextauth]/options.js
+++ src/app/api/auth/[...nextauth]/options.js
@@ -1,12 +1,12 @@
 
-import  { NextAuthOptions } from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
+// import  { NextAuthOptions } from "next-auth";
+// import GitHubProvider from "next-auth/providers/github";
 
-export const options: NextAuthOptions = {
-    providers: [
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET
-        })
-    ]
-};
+// export const options: NextAuthOptions = {
+//     providers: [
+//         GitHubProvider({
+//             clientId: process.env.GITHUB_ID,
+//             clientSecret: process.env.GITHUB_SECRET
+//         })
+//     ]
+// };
--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,35 @@
 import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+import GoogleProvider from "next-auth/providers/google"
+const authOptions = {
+    providers: [
+        CredentialsProvider({
 
-const handler = NextAuth({
-  
-})
+            name: 'Email',
+            secret: process.env.NEXTAUTH_SECRET,
+            credentials: {
+                username: { label: "Username", type: "text", placeholder: "jsmith" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                console.log(credentials, "credentials")
+
+                const user = { email: "ali@gmail.com", fullname: "ali ahmad" }
+                if (user) {
+                    return user
+                }
+                // Return null if user data could not be retrieved
+                return null
+            }
+        }),
+        // GoogleProvider({
+        //     clientId: process.env.GOOGLE_CLIENT_ID,
+        //     clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        // })
+    ]
+}
+
+const handler = NextAuth(authOptions)
+
 
 export { handler as GET, handler as POST }

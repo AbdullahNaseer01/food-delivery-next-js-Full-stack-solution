48be19565fd2e201a7dabcbde0e55d655b94ccf3
abdullahnaseer01 <mianabdullah125125@gmail.com>
2023-12-29 00:45:15 +0500

    refactoring

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,48 +1,3 @@
-// import NextAuth from "next-auth"
-// import CredentialsProvider from "next-auth/providers/credentials"
-// import GoogleProvider from "next-auth/providers/google";
-
-// const authOption = {
-//     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-//     providers: [
-//         CredentialsProvider({
-//             secret: process.env.NEXTAUTH_SECRET,
-//             name: 'Email',
-//             credentials: {
-//                 username: { label: "Username", type: "text", placeholder: "jsmith" },
-//                 password: { label: "Password", type: "password" }
-//             },
-//             async authorize(credentials, req) {
-//                 // const res = await fetch("/your/endpoint", {
-//                 //   method: 'POST',
-//                 //   body: JSON.stringify(credentials),
-//                 //   headers: { "Content-Type": "application/json" }
-//                 // })
-//                 // const user = await res.json()
-//                 console.log("credentials", credentials)
-//                 // some ORM function here to compare the user with the database and check is there anyone with these things then allow him to go further
-//                 const user = { email: "loremipsem@gmail.com", fullName: "mian abdullah", password: "123456" }
-//                 if (user) {
-//                     return user
-//                 }
-//                 return null
-//             }
-//         }),
-//         GoogleProvider({
-//             clientId: process.env.GOOGLE_CLIENT_ID,
-//             clientSecret: process.env.GOOGLE_CLIENT_SECRET
-//         })
-//     ]
-// }
-
-// const handler = NextAuth(authOption)
-
-// export { handler as GET, handler as POST }
-
-
-
-
-
 import dbConnect from "@/config/db";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
@@ -85,3 +40,54 @@ const authOption = {
 const handler = NextAuth(authOption);
 
 export { handler as GET, handler as POST };
+
+
+
+
+
+// import dbConnect from "@/config/db";
+// import NextAuth from "next-auth";
+// import CredentialsProvider from "next-auth/providers/credentials";
+// import { User } from "@/app/models/user";
+
+// dbConnect();
+
+// const authOptions = {
+//   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+//   providers: [
+//     CredentialsProvider({
+//       secret: process.env.NEXTAUTH_SECRET,
+//       name: 'Email',
+//       credentials: {
+//         username: { label: "Username", type: "text", placeholder: "jsmith" },
+//         password: { label: "Password", type: "password" }
+//       },
+//       async authorize(credentials, req) {
+//         try {
+//           const user = await User.findOne({ email: credentials.username });
+
+//           if (user && user.password === credentials.password) {
+//             // Include the user's role in the returned object
+//             const role = user.email === "mianabdullah12512@gmail.com" ? "admin" : "user";
+//             return { ...user._doc, role };
+//           }
+//         } catch (error) {
+//           console.error('Error fetching user:', error);
+//         }
+
+//         return null;
+//       }
+//     }),
+//   ],
+//   callbacks: {
+//     async session(session, user) {
+//       // Include the user's role in the session object
+//       session.role = user.role;
+//       return Promise.resolve(session);
+//     }
+//   }
+// };
+
+// const handler = NextAuth(authOptions);
+
+// export { handler as GET, handler as POST };
--- src/middleware.js
+++ src/middleware.js
@@ -22,7 +22,7 @@ import { withAuth } from "next-auth/middleware"
 export default withAuth(
   // `withAuth` augments your `Request` with the user's token.
   function middleware(req) {
-    console.log(req.nextauth.token)
+    console.log("token:", req.nextauth.token)
   },
   {
     secret:process.env.NEXTAUTH_SECRET

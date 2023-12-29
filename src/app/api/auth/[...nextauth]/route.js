f2673d154b3f8f1b7cea4daa1bee86547aab7107
abdullahnaseer01 <mianabdullah125125@gmail.com>
2023-12-29 20:31:40 +0500

    role based auth is done middleware is pending

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,3 +1,73 @@
+// import dbConnect from "@/config/db";
+// import NextAuth from "next-auth";
+// import CredentialsProvider from "next-auth/providers/credentials";
+// import GoogleProvider from "next-auth/providers/google";
+// import { User } from "@/app/models/user";
+
+// dbConnect();
+
+// const authOption = {
+//     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+//     providers: [
+//         CredentialsProvider({
+//             secret: process.env.NEXTAUTH_SECRET,
+//             name: 'Email',
+//             credentials: {
+//                 username: { label: "Username", type: "text", placeholder: "jsmith" },
+//                 password: { label: "Password", type: "password" }
+//             },
+//             async authorize(credentials, req) {
+//                 try {
+//                     const user = await User.findOne({ email: credentials.username });
+
+//                     if (user && user.password === credentials.password) { // Direct comparison (not secure)
+//                         return user;
+//                     }
+//                 } catch (error) {
+//                     console.error('Error fetching user:', error);
+//                 }
+
+//                 return null;
+//             }
+//         }),
+//         GoogleProvider({
+//             profile(profile) {
+//                 console.log("Google profile:", profile)
+
+//                 let userRole = "Google User"
+//                 if (profile?.email == "mianabdullah125125@gmail.com") {
+//                     userRole = "admin"
+//                 }
+//                 return {
+//                     ...profile,
+//                     id: profile.sub,
+//                     role: userRole,
+//                 }
+//             },
+//             clientId: process.env.GOOGLE_CLIENT_ID,
+//             clientSecret: process.env.GOOGLE_CLIENT_SECRET
+//         })
+//     ],
+//     callbacks: {
+//         async jwt({ token, user }) {
+//             if (user) token.role = user.role
+//             return token
+//         },
+//         async session({ token, user }) {
+//             if (session?.user) session.user.role = token.role
+//             return session
+//         }
+//     }
+// }
+
+// const handler = NextAuth(authOption);
+
+// export { handler as GET, handler as POST };
+
+
+
+
+
 import dbConnect from "@/config/db";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
@@ -20,8 +90,10 @@ const authOption = {
                 try {
                     const user = await User.findOne({ email: credentials.username });
 
-                    if (user && user.password === credentials.password) { // Direct comparison (not secure)
-                        return user;
+                    if (user && user.password === credentials.password) {
+                        // Include the user's role in the returned object
+                        const role = user.email === "mianabdullah125125@gmail.com" ? "admin" : "client";
+                        return { ...user, role }; // Corrected this line
                     }
                 } catch (error) {
                     console.error('Error fetching user:', error);
@@ -31,10 +103,33 @@ const authOption = {
             }
         }),
         GoogleProvider({
+            profile(profile) {
+                console.log("Google profile:", profile)
+
+                let userRole = "Google User"
+                if (profile?.email == "mianabdullah125125@gmail.com") {
+                    userRole = "admin"
+                }
+                return {
+                    ...profile,
+                    id: profile.sub,
+                    role: userRole,
+                }
+            },
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
-    ]
+    ],
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) token.role = user.role
+            return token
+        },
+        async session({ token, user }) {
+            if (session?.user) session.user.role = token.role
+            return session
+        }
+    }
 }
 
 const handler = NextAuth(authOption);
@@ -45,49 +140,5 @@ export { handler as GET, handler as POST };
 
 
 
-// import dbConnect from "@/config/db";
-// import NextAuth from "next-auth";
-// import CredentialsProvider from "next-auth/providers/credentials";
-// import { User } from "@/app/models/user";
 
-// dbConnect();
 
-// const authOptions = {
-//   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-//   providers: [
-//     CredentialsProvider({
-//       secret: process.env.NEXTAUTH_SECRET,
-//       name: 'Email',
-//       credentials: {
-//         username: { label: "Username", type: "text", placeholder: "jsmith" },
-//         password: { label: "Password", type: "password" }
-//       },
-//       async authorize(credentials, req) {
-//         try {
-//           const user = await User.findOne({ email: credentials.username });
-
-//           if (user && user.password === credentials.password) {
-//             // Include the user's role in the returned object
-//             const role = user.email === "mianabdullah12512@gmail.com" ? "admin" : "user";
-//             return { ...user._doc, role };
-//           }
-//         } catch (error) {
-//           console.error('Error fetching user:', error);
-//         }
-
-//         return null;
-//       }
-//     }),
-//   ],
-//   callbacks: {
-//     async session(session, user) {
-//       // Include the user's role in the session object
-//       session.role = user.role;
-//       return Promise.resolve(session);
-//     }
-//   }
-// };
-
-// const handler = NextAuth(authOptions);
-
-// export { handler as GET, handler as POST };
--- src/middleware.js
+++ src/middleware.js
@@ -1,32 +1,71 @@
-// import { withAuth } from "next-auth/middleware"
-
-// export default withAuth(
-//   // `withAuth` augments your `Request` with the user's token.
-//   function middleware(req) {
-//     console.log(req.nextauth.token)
-//   },
-//   {
-//     callbacks: {
-//       authorized: ({ token }) => token?.role === "admin",
-//     },
-//   }
-// )
-
-// export const config = { matcher: ["/admin"] }
-
-
-
-
 import { withAuth } from "next-auth/middleware"
 
 export default withAuth(
   // `withAuth` augments your `Request` with the user's token.
   function middleware(req) {
-    console.log("token:", req.nextauth.token)
+    console.log("token:",req.nextauth.token)
   },
   {
-    secret:process.env.NEXTAUTH_SECRET
+    callbacks: {
+      authorized: ({ token }) => token?.role === "admin",
+    },
   }
 )
 
-export const config = { matcher: ["/adminpanel","/profile"] }
+export const config = { matcher: ["/adminpanel",] }
+
+
+
+
+// import { withAuth } from "next-auth/middleware"
+
+// export default withAuth(
+//   // `withAuth` augments your `Request` with the user's token.
+//   function middleware(req) {
+//     console.log("token:", req.nextauth.token)
+//   },
+//   {
+//     secret:process.env.NEXTAUTH_SECRET
+//   }
+// )
+
+// export const config = { matcher: ["/adminpanel","/profile"] }
+
+
+
+
+
+
+
+// // "this code is not working and its exparimental"
+// import { withAuth } from "next-auth/middleware";
+// import { NextResponse } from "next/server";
+
+
+// export default withAuth(
+//   async function middleware(req) {
+//     const token = req.nextauth.token;
+//     const role = token?.role;
+//     console.log("token:", req.nextauth.token)
+
+//     const protectedRoutes = {
+//       "/adminpanel": ["admin"],
+//       "/profile": ["*"],
+//     };
+
+//     const allowedRoles = protectedRoutes[req.nextUrl.pathname];
+
+//     if (allowedRoles && !allowedRoles.includes(role)) {
+//       NextResponse.redirect(new URL('/', req.url));
+//     }
+
+//     if (!token) {
+//       NextResponse.redirect(new URL('/login', req.url));
+//     }
+//   },
+//   {
+//     secret: process.env.NEXTAUTH_SECRET,
+//   }
+// );
+
+// export const config = { matcher: ["/adminpanel", "/profile"] };
